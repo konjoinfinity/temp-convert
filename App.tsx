251eb991a16007ee54b08651642ee74a8bbe519b
Wesley Scholl <clients@konjoweb.com>
2023-01-24 13:14:50 -0500

    conversion function logic

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 
 interface Props {
   name: string
-  value: string
+  value: number
   func: Function
   icon: any
 }
@@ -18,7 +18,7 @@ const Input: React.FC<Props> = ({name, value, func, icon}) => {
     label={name}
     variant="outlined"
     keyboardType='decimal-pad'
-    value={value}
+    value={`${value}`}
     onChangeText={deg => func(deg)}
     blurOnSubmit={false}
     trailing={props => (
@@ -27,23 +27,35 @@ const Input: React.FC<Props> = ({name, value, func, icon}) => {
 }
 
 export default function App() {
-  const [fahrenheit, setFahrenheit] = useState('')
-  const [celsius, setCelsius] = useState('')
-  const [kelvin, setKelvin] = useState('')
+  const [fahrenheit, setFahrenheit] = useState(32)
+  const [celsius, setCelsius] = useState(0)
+  const [kelvin, setKelvin] = useState(273.15)
 
   const fToCandK = () => {
     // F to C => (68°F - 32) × 5/9 = 20 °C
+    let convertFtoC = (fahrenheit - 32) * 5/9
+    setCelsius(convertFtoC)
     // F to K => (60°F + 459.67) × 5/9 = 288.71 K
+    let convertFtoK = (fahrenheit + 459.67) * 5/9
+    setKelvin(convertFtoK)
         }
   
   const cToFandK = () => {
     // C to F => 20°C × 9/5 + 32 = 68 °F
+    let convertCtoF = celsius * 9/5 + 32
+    setFahrenheit(convertCtoF)
     // C to K => 10°C + 273.15 = 283.15 K
+    let convertCtoK = celsius + 273.15
+    setKelvin(convertCtoK)
   }
         
   const kToCandF = () => {
     // K to C => 300K - 273.15 = 26.85 °C
-        // K to F => 300K × 9/5 - 459.67 = 80.33 °F
+    let convertKtoC = kelvin - 273.15
+    setCelsius(convertKtoC)
+    // K to F => 300K × 9/5 - 459.67 = 80.33 °F
+    let convertKtoF = kelvin * 9/5 - 459.67
+    setFahrenheit(convertKtoF)
   }
         
 
