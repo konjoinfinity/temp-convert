6dba54cdcb2e87db4c473e34d9c43ddd46bb1277
Wesley Scholl <clients@konjoweb.com>
2023-01-24 21:24:28 -0500

    updated logic, determined that state was storing values as strings, coverted to Numbers, functional

--- App.tsx
+++ App.tsx
@@ -1,18 +1,19 @@
 
-import { StyleSheet, Text, View, Dimensions, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { View, Dimensions, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import React, { useState } from 'react';
 import { Button, TextInput, IconButton } from "@react-native-material/core";
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
-
+import { Shadow } from 'react-native-shadow-2';
 
 interface Props {
   name: string
   value: number
   set: Function
   icon: any
+  unit: Function
 }
 
-const Input: React.FC<Props> = ({name, value, set, icon}) => {
+const Input: React.FC<Props> = ({name, value, set, icon, unit}) => {
   return (
     <TextInput
     style={{width: Dimensions.get('screen').width * 0.8}}
@@ -20,7 +21,8 @@ const Input: React.FC<Props> = ({name, value, set, icon}) => {
     textAlign='right'
     keyboardType='decimal-pad'
     value={`${value}`}
-    onChangeText={deg => {set(deg)}}
+    onChangeText={deg => set(deg)}
+    onPressIn={() => unit(name)}
     blurOnSubmit={false}
     trailing={props => (
     <IconButton icon={props => <Icon name={icon} {...props} />} {...props} />)} />
@@ -31,41 +33,38 @@ export default function App() {
   const [fahrenheit, setFahrenheit] = useState(32)
   const [celsius, setCelsius] = useState(0)
   const [kelvin, setKelvin] = useState(273.15)
+  const [unit, setUnit] = useState('')
 
-  const convertAll = () => {
-    // if(celsius == 0 && kelvin == 237.15)
-    setCelsius((fahrenheit - 32) * 0.5556)
-    console.log(fahrenheit + 459.67)
-    setKelvin((fahrenheit + 459.67) * 0.5556)
-    // let convertCtoF = celsius * 9/5 + 32
-    // setFahrenheit(Math.round(convertCtoF*10)/10)
-    // let convertCtoK = celsius + 273.15
-    // setKelvin(Math.round(convertCtoK*10)/10)
-    // let convertKtoC = kelvin - 273.15
-    // setCelsius(Math.round(convertKtoC*10)/10)
-    // let convertKtoF = kelvin * 9/5 - 459.67
-    // setFahrenheit(Math.round(convertKtoF*10)/10)
+  const fToCandK = () => {
+    let fToC = Number(fahrenheit) - 32 * 0.5556
+    setCelsius(Math.ceil(fToC * 100) / 100)
+    let fToK = 459.67 * 0.5556 + Number(fahrenheit)
+    setKelvin(Math.ceil(fToK * 100) / 100)
   }
-        
 
+  const cToFandK = () => {
+    let cToF = Number(celsius) / 0.5556 + 32
+    setFahrenheit(Math.ceil(cToF * 100) / 100)
+    let cToK = Number(celsius) + 273.15
+    setKelvin(Math.ceil(cToK * 100) / 100)
+  }
+
+  const kToFandC = () => {
+    let kToF = Number(kelvin) * 1.8 - 459.67
+    setFahrenheit(Math.ceil(kToF * 100) / 100)
+    let kToC = Number(kelvin) - 273.15
+    setCelsius(Math.ceil(kToC * 100) / 100)
+  }
+  
   return (
-
     <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-    <View style={styles.container}>
-      <Input name={'Fahrenheit'} value={fahrenheit} set={setFahrenheit} icon={'temperature-fahrenheit'}/>
-      <Input name={'Celsius'} value={celsius} set={setCelsius} icon={'temperature-celsius'}/>
-      <Input name={'Kelvin'} value={kelvin} set={setKelvin} icon={'temperature-kelvin'}/>
-      <Button title="Convert" onPress={() => convertAll()} style={{marginBottom: Dimensions.get('screen').height * 0.1, marginTop:10, padding: 10}}/>
+      <View style={{flex: 1,backgroundColor: '#000',alignItems: 'center',justifyContent: 'center'}}>
+      <Input name={'Fahrenheit'} value={fahrenheit} set={setFahrenheit} icon={'temperature-fahrenheit'} unit={setUnit}/>
+      <Input name={'Celsius'} value={celsius} set={setCelsius} icon={'temperature-celsius'} unit={setUnit}/>
+      <Input name={'Kelvin'} value={kelvin} set={setKelvin} icon={'temperature-kelvin'} unit={setUnit}/>
+      <Button title="Convert" onPress={() => unit == 'Fahrenheit' ? fToCandK() : unit == 'Celsius' ? cToFandK() : kToFandC()} 
+      style={{marginBottom: Dimensions.get('screen').height * 0.05, marginTop:10, padding: 10}}/>
     </View>
     </TouchableWithoutFeedback>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
